34c62fc04b871360390673e8268624916e316365
Lucas Ferreira da Silva <ferreira.silva5641@gmail.com>
2023-02-26 23:54:16 -0300

    create carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import * as data from '../assets/teste.json';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'carousel-app';
+
+  filterContent: any[] = [];
+
+  all: any[] = [];
+  content = data;
+
+  constructor() { }
+
+  ngOnInit() {
+    for (let i = 0; i < this.content.length; i++) {
+      i === 0 ? this.all.push(this.content[i]) : this.filterContent.push(this.content[i])
+    }
+
+    console.log(this.filterContent)
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ButtonModule,
+    CarouselModule,
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
+
 export class AppModule { }
